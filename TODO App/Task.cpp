0e2a7dad69972c0fd4d093896a25d7d4705b0c79
hrshdp <harshdeepchandrakar9@gmail.com>
2025-05-31 09:02:57 +0530

    Added declarations in Task.h and Added methods in Task.cpp

---
+++ TODO App/Task.cpp
@@ -0,0 +1,47 @@
+﻿#include "Task.h"
+#include <iostream>
+
+
+// Constructor to initialize the task with a description and status
+Task::Task(int taskId, const std::string& taskDescription): id(taskID), description(TaskDescription), isCompleted(false) {}
+
+// Getter for Task ID
+int Task::getId() const {
+	return id;
+}
+
+
+// Getter for Task Description
+std::string Task::getDescription() const {
+	return description;
+}
+
+// Getter for completion status
+bool Task::getCompletionStatus() const {
+	return isCompleted;
+}
+
+
+// Set new description
+void Task::setDescription(const std::string& newDescription) {
+	description = newDescription;
+}
+
+// Mark task as complete 
+void Task::markCompleted() {
+	isCompleted = true;
+}
+
+// Mark task as incomplete
+void Task::markIncomplete() {
+	isCompleted = false;
+}
+
+// Print task info
+void Task::display() const {
+	std::cout << "[" << (isCompleted ? "✔" : "✘") << "] " << "(" << id << ") " << description << std::endl;
+}
+
+
+
+
--- TODO App/Task.h
+++ TODO App/Task.h
@@ -1,2 +1,29 @@
-#pragma once
+#pragma once // this ensures that this file is included only once during compilation
+
+#include <string>
+
+class Task {
+private:
+	int id;
+	std::string desription;
+	bool isDone;
+public:
+	// Constructor to initialize a Task with an ID and description
+	Task(int taskId, const std::string taskDescription);
+
+	
+	// Getters
+	int getId() const; // const at the end indicates that this method does not change anything in the function
+	std::string getDescription() const; 
+	bool getCompletionStatus() const;
+
+
+	// Setters
+	void setDescription(const std::string& newDescription);
+	void markCompleted();
+	void markIncomplete();
+
+	// Method to display the task details
+	void display() const;	
+};
 
